8cd9cc92cbe051a1cdb2fd90a28941ece0ac56a0
Dewald Els <dewaldifels@gmail.com>
2025-05-06 17:48:01 +0200

    feat: Go to's, Format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,27 +7,32 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Split window
-map("n", "<leader>sv", "<C-w>v", opts) -- Split window veritcally
-map("n", "<leader>sh", "<C-w>s", opts) -- Split window horizontally
-map("n", "<leader>se", "<C-w>=", opts) -- Make split equal
+map("n", "<leader>sv", "<C-w>v", opts)     -- Split window veritcally
+map("n", "<leader>sh", "<C-w>s", opts)     -- Split window horizontally
+map("n", "<leader>se", "<C-w>=", opts)     -- Make split equal
 map("n", "<leader>sx", ":close<CR>", opts) -- Close current split
 
 -- Buffer navigation
-map("n", "<leader>bn", ":bnext<CR>", opts) -- Next Buffer
+map("n", "<leader>bn", ":bnext<CR>", opts)     -- Next Buffer
 map("n", "<leader>bp", ":bprevious<CR>", opts) -- Prev Buffer
-map("n", "<leader>bx", ":bdelete<CR>", opts) -- Delete current buffer
+map("n", "<leader>bx", ":bdelete<CR>", opts)   -- Delete current buffer
 
 -- File navigation
-map("n", "<leader>fe", ":Explore<CR>", opts) -- Open the File explrorer 
+map("n", "<leader>fe", ":Explore<CR>", opts)  -- Open the File explrorer
 map("n", "<leader>fv", ":Vexplore<CR>", opts) -- Open explorer in side
 
--- FZF File search 
-map("n", "<leader>sf", ":Files<CR>", opts) -- Search files
-map("n", "<leader>sb", ":Buffers<CR>", opts) -- Search buffers
-map("n", "<leader>so", ":BLines<CR>", opts) -- Search current file
+-- FZF File search
+map("n", "<leader>sf", ":Files<CR>", opts)            -- Search files
+map("n", "<leader>sb", ":Buffers<CR>", opts)          -- Search buffers
+map("n", "<leader>so", ":BLines<CR>", opts)           -- Search current file
 map("n", "<leader>sa", ":Rg<CR>", { noremap = true }) -- Search project with Ripgrep
-map("n", "<leader>sh", ":History<CR>", opts) -- Search history
+map("n", "<leader>sh", ":History<CR>", opts)          -- Search history
 
 -- Code actions
 map("n", "K", vim.lsp.buf.hover, { noremap = true })
 map("n", "<C-k>", vim.lsp.buf.signature_help, { noremap = true })
+map("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
+map("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true })
+map("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true })
+map("n", "gi", vim.lsp.buf.implementation, { noremap = true, silent = true })
+map("n", "<C-S-F>", function() vim.lsp.buf.format({ async = true }) end, { noremap = true, silent = true })
